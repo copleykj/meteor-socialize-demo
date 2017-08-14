15aa415105ab24cdbeaeed1491fe295ceb7df730
Kelly Copley <copleykj@gmail.com>
2017-08-13 23:06:28 -0400

    setup user presence

---
+++ server/presence.js
@@ -0,0 +1,32 @@
+import { Meteor } from 'meteor/meteor';
+import { User } from 'meteor/socialize:user-model';
+import { UserPresence } from 'meteor/socialize:user-presence';
+import SimpleSchema from 'simpl-schema';
+
+
+const StatusSchema = new SimpleSchema({
+    status: {
+        type: String,
+        allowedValues: ['online', 'idle'],
+    },
+});
+
+User.attachSchema(StatusSchema);
+
+UserPresence.onCleanup(function onCleanup(sessionIds) {
+    if (!sessionIds) {
+        Meteor.users.update({}, { $unset: { status: true } }, { multi: true });
+    }
+});
+
+UserPresence.onUserOnline(function onUserOnline(userId) {
+    Meteor.users.update(userId, { $set: { status: 'online' } });
+});
+
+UserPresence.onUserIdle(function onUserIdle(userId) {
+    Meteor.users.update(userId, { $set: { status: 'idle' } });
+});
+
+UserPresence.onUserOffline(function onUserOffline(userId) {
+    Meteor.users.update(userId, { $unset: { status: true } });
+});
