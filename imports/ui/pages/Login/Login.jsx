df6ae8940abdaccafb4d400f5819485cde900338
Kelly Copley <copleykj@gmail.com>
2017-08-14 09:32:49 -0400

    bug fixes and error reporting in login form

--- imports/ui/pages/Login/Login.jsx
+++ imports/ui/pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import { User } from 'meteor/socialize:user-model';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { browserHistory } from 'react-router';
-import { Grid, Row, Col, Button, Panel } from 'react-bootstrap';
+import { Grid, Row, Col, Button, Panel, Collapse } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import AutoField from 'uniforms-bootstrap3/AutoField';
 import AutoForm from 'uniforms-bootstrap3/AutoForm';
@@ -38,7 +38,7 @@ export default class LoginPage extends Component {
         user: PropTypes.instanceOf(User),
     }
 
-    state = { signUp: false };
+    state = { signUp: false, loginError: null };
 
     componentWillMount() {
         if (this.props.user) {
@@ -47,22 +47,33 @@ export default class LoginPage extends Component {
     }
 
     handleSubmit = (doc) => {
+        this.setState({ loginError: null });
         if (this.state.signUp) {
             Accounts.createUser({ ...doc }, (err) => {
                 if (!err) {
                     browserHistory.replace('/');
+                } else {
+                    this.setState({ loginError: new Error(err.reason) });
+                }
+            });
+        } else {
+            Meteor.loginWithPassword(doc.usernameOrEmail, doc.password, (err) => {
+                if (!err) {
+                    browserHistory.replace('/');
+                } else {
+                    this.setState({ loginError: new Error('Invalid username or password') });
                 }
             });
         }
-        Meteor.loginWithPassword(doc.usernameOrEmail, doc.password, (err) => {
-            if (!err) {
-                browserHistory.replace('/');
-            }
-        });
+    }
+
+    switchForm = (event) => {
+        event.preventDefault();
+        this.setState({ signUp: !this.state.signUp, loginError: null });
     }
 
     render() {
-        const { signUp } = this.state;
+        const { signUp, loginError } = this.state;
         return (
             <Grid>
                 <Row>
@@ -70,22 +81,26 @@ export default class LoginPage extends Component {
                         <h1 className="text-center" style={{ marginTop: '200px' }}>Socialize</h1>
                         <Panel header={signUp ? 'Register Account' : 'Login'} bsStyle="primary">
                             { signUp &&
-                                <AutoForm schema={RegisterSchema} onSubmit={this.handleSubmit}>
+                                <AutoForm showInlineError schema={RegisterSchema} onSubmit={this.handleSubmit} error={loginError}>
                                     <AutoField name="username" />
                                     <AutoField name="email" />
                                     <AutoField name="password" type="password" />
+                                    <Collapse in={!!loginError}>
+                                        <ErrorsField />
+                                    </Collapse>
                                     <Button type="submit" bsStyle="primary">Register</Button>
-                                    <ErrorsField />
-                                    <p style={{ marginTop: '1.2em' }}>Already have an account? <a href="#" onClick={(event) => { event.preventDefault(); this.setState({ signUp: false }); }}>Log In</a></p>
+                                    <p style={{ marginTop: '1.2em' }}>Already have an account? <a href="#" onClick={this.switchForm}>Log In</a></p>
                                 </AutoForm>
                             }
                             { !signUp &&
-                                <AutoForm schema={LoginSchema} onSubmit={this.handleSubmit}>
+                                <AutoForm showInlineError schema={LoginSchema} onSubmit={this.handleSubmit} error={loginError}>
                                     <AutoField name="usernameOrEmail" label="Username or Email" />
                                     <AutoField name="password" type="password" />
+                                    <Collapse in={!!loginError}>
+                                        <ErrorsField />
+                                    </Collapse>
                                     <Button type="submit" bsStyle="primary">Log In</Button>
-                                    <ErrorsField />
-                                    <p style={{ marginTop: '1.2em' }}>Don&apos;t have an account? <a href="#" onClick={(event) => { event.preventDefault(); this.setState({ signUp: true }); }}>Register</a></p>
+                                    <p style={{ marginTop: '1.2em' }}>Don&apos;t have an account? <a href="#" onClick={this.switchForm}>Register</a></p>
                                 </AutoForm>
                             }
                         </Panel>
